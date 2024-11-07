379830a7c2753a494c3675fbc37ca98f1be63e93
Xiomara18b <betancurxiomara0@gmail.com>
2024-11-07 10:57:56 -0500

    Adding Components Rest API.

--- src/main/java/culturemedia/controller/CultureMediaController.java
+++ src/main/java/culturemedia/controller/CultureMediaController.java
@@ -5,23 +5,29 @@ import java.util.*;
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
 import culturemedia.service.CultureMediaService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 public class CultureMediaController {
 
     private final CultureMediaService cultureMediaService;
-
 
     public CultureMediaController(CultureMediaService cultureMediaService) {
         this.cultureMediaService = cultureMediaService;
     }
 
-
+    @GetMapping("/videos")
     public List<Video> findAllVideos() throws VideoNotFoundException {
         List<Video> videos = cultureMediaService.findAll();
         return videos;
     }
 
-    public Video save(Video video){
+    @PostMapping("/videos")
+    public Video save(@RequestBody Video video){
         cultureMediaService.save(video);
         return video;
     }
